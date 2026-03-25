8f182fed6bb238f01c84e13b1280a6fb6a759127
Guilherme Henrique <guihenrique.s@outlook.com>
2026-03-25 14:03:29 -0300

    first commit

---
+++ TableFIP/src/main/java/com/example/TableFIP/model/Dados.java
@@ -0,0 +1,4 @@
+package com.example.TableFIP.model;
+
+public record Dados (String codigo, String nome) {
+}
---
+++ TableFIP/src/test/java/com/example/TableFIP/TableFipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TableFIP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableFipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
